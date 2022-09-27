62699ebf6a559fce03cab498465d0bd0355f4442
nyxspi <nyxspi@gmail.com>
2022-09-27 22:23:05 +0200

    Added Required files, Added Connection String, Added 3 repository classes for Devices, Categories and Zones.

---
+++ ConnectedOffice/DeviceManagement_WebApp/Repository/ZoneRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeviceManagement_WebApp.Repository
+{
+    public class ZoneRepository
+    {
+    }
+}
